7974add530af6b52acb94d5011d309be5d10c9d7
Mohammed Shahid <ofcacc0324@gmail.com>
2025-12-15 15:12:15 +0530

    Loading correct file

--- backend/auth.py
+++ backend/auth.py
@@ -11,7 +11,7 @@ from dotenv import load_dotenv
 from db import get_session
 
 # Load environment variables
-load_dotenv(".env.development")
+load_dotenv(".env")
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 10080))
