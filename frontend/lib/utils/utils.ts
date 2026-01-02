e677abc58ba3e222c8f97304eeb5765b495cdac6
Mohammed Shahid <ofcacc0324@gmail.com>
2026-01-02 16:36:25 +0530

    Frontend: Added time conversion util

--- frontend/lib/utils/utils.ts
+++ frontend/lib/utils/utils.ts
@@ -1,6 +1,18 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function parseBackendDate(dateString: string) {
+  // If the string is missing the 'Z' (UTC marker) and has no timezone offset, append 'Z'
+  if (
+    dateString &&
+    !dateString.endsWith("Z") &&
+    !/[+-]\d{2}:\d{2}$/.test(dateString)
+  ) {
+    return new Date(dateString + "Z");
+  }
+  return new Date(dateString);
 }
